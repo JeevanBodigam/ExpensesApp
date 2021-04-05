a69df25c017c254474372bf8a1094acec8740505
jeevan bodigam <bodigamjeevan296@gmail.com>
2021-04-05 17:05:38 -0500

    Initial Commit

---
+++ ToDo App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo App
+//
+//  Created by Bodigam,Jeevan on 4/5/21.
+//  Copyright © 2021 Bodigam,Jeevan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
